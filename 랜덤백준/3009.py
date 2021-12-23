e0a1ae68610e496d8887b8863f04c8a1e4222950
Pretty Han <imbel9830@gmail.com>
2021-12-23 23:12:35 +0900

    1st

--- 랜덤백준/3009.py
+++ 랜덤백준/3009.py
@@ -1,1 +1,14 @@
 # 네 번째 점
+A = []
+B = []
+
+for i in range(3):
+    a, b = map(int, input().split())
+    A.append(a)
+    B.append(b)
+for ans in range(3):
+    if A.count(A[ans]) == 1: # A[0,1,2] < 3개 다돌려서 count 1 된거 찾음
+        ans1 = A[ans]
+    if B.count(B[ans]) == 1:
+        ans2 = B[ans]
+print(ans1, ans2)         
