634215b453b943a27e657f9a5027ba743eaf5999
PrettyHan <imbel9830@gmail.com>
2022-08-14 16:59:21 +0900

    add: 2108 시간초과

--- 정렬/2108.py
+++ 정렬/2108.py
@@ -1,12 +1,35 @@
-input_list = []
+from collections import Counter
 
+input_list = []
 
 for i in range((int(input()))):
     input_list.append((int(input())))
 
+input_list.sort()
+
 def solve(input_list):
-    print(round(sum(input_list) / len(input_list)))
     mid = len(input_list) // 2
+    print(round(sum(input_list) / len(input_list)))
     print(input_list[mid])
-    
+    print(mode(input_list))
+    print(range(input_list))
+
+
+def mode(input_list):
+    counted_list = Counter(input_list).most_common()
+    max = counted_list[0][1]
+    modes = []
+    for i in counted_list:
+        if i[1] == max:
+            modes.append(i[0])  
+    if len(modes) >= 2:
+        modes.sort()
+        return modes[1]
+    return modes[0]
+
+def range(input_list):
+    max_num = max(input_list)
+    min_num = min(input_list)
+    return max_num - min_num
+
 solve(input_list)
