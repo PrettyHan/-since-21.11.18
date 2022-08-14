c77c88940a330763e7f1a42bf5fb0696f1de16b2
PrettyHan <imbel9830@gmail.com>
2022-08-14 17:06:49 +0900

    fix: 2108 시간초과 -2

--- 정렬/2108.py
+++ 정렬/2108.py
@@ -1,8 +1,9 @@
+import sys
 from collections import Counter
 
 input_list = []
 
-for i in range((int(input()))):
+for i in range((int(sys.stdin.readline()))):
     input_list.append((int(input())))
 
 input_list.sort()
@@ -18,14 +19,21 @@ def solve(input_list):
 def mode(input_list):
     counted_list = Counter(input_list).most_common()
     max = counted_list[0][1]
-    modes = []
-    for i in counted_list:
-        if i[1] == max:
-            modes.append(i[0])  
-    if len(modes) >= 2:
-        modes.sort()
-        return modes[1]
-    return modes[0]
+    # modes = []
+    # for i in counted_list:
+    #     if i[1] == max:
+    #         modes.append(i[0])  
+    # if len(modes) >= 2:
+    #     modes.sort()
+    #     return modes[1]
+    # return modes[0]
+    if len(counted_list) > 1:
+        if max == counted_list[1][1]:
+            return counted_list[1][0]
+        else:
+            return counted_list[0][0]
+    else:
+        return counted_list[0][0]
 
 def range(input_list):
     max_num = max(input_list)
