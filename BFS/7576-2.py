c01f9f3db341e6eb0908d7937a8c2a4b2e6afbd8
imbel9830 <imbel9830@gmail.com>
2022-11-21 14:37:06 +0900

    백준: 7576 토마토

---
+++ BFS/7576-2.py
@@ -0,0 +1,32 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+n,m = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(m)]
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs():
+    queue = deque()
+    for i in range(m):
+        for j in range(n):
+            if graph[i][j] == 1:
+                queue.append((i,j))
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or nx >= m or ny < 0 or ny >= n:
+                continue
+            if graph[nx][ny] == 0:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+    for j in graph:
+        if 0 in j:
+            return -1
+    return max(map(max, graph)) - 1
+
+print(bfs())
