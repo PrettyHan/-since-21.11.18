599f701a06ac43d042f443e9eaae4838853b63fd
imbel9830 <imbel9830@gmail.com>
2023-01-02 20:52:48 +0900

    add : 2493 주석

--- 스택and큐/2493.py
+++ 스택and큐/2493.py
@@ -7,15 +7,15 @@ stack = []
 answer = [0] * n
 
 for i in range(n):
-    send = signal[i]
+    send = signal[i] # [6 9 5 7 4]
     while stack:
-        if send < stack[-1][1]:
-            answer[i] = stack[-1][0] + 1
+        if send < stack[-1][1]: # 수신 가능
+            answer[i] = stack[-1][0] + 1 # 인덱스
             break
-        else:
+        else: # 수신불가
             answer[i] = 0
             stack.pop()
-    stack.append([i, send])
+    stack.append([i, send]) #([6, 0])
 
 print(*answer)
         
